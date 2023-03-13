d1897c9c9ae6d4f8c4cd1b0edce14e7e9ea3b7ad
DarkHorseCoder <120799088+DarkHorseCorder@users.noreply.github.com>
2023-03-13 17:25:33 +0900

    init setup

---
+++ Portfolio-Server/src/data/projects.js
@@ -0,0 +1,172 @@
+const projects = [
+  {
+    title: "Palm Oil 360 game",
+    image:
+      "https://ik.imagekit.io/itsrakesh/Portfolio/pika-1678164126072-1x_-aQOmkd1TG.png?ik-sdk-version=javascript-1.4.3&updatedAt=1678164205176",
+    repoName: "",
+    liveUrl: "https://palm-oil-itsrakesh.vercel.app/",
+    techs: [
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        title: "React",
+      },
+      {
+        url: "https://vitejs.dev/logo.svg",
+        title: "Vite",
+      },
+      {
+        url: "https://pbs.twimg.com/profile_images/1244925541448286208/rzylUjaf_400x400.jpg",
+        title: "Chakra-UI",
+      },
+    ],
+  },
+  {
+    title: "TickleLife Website",
+    image:
+      "https://ik.imagekit.io/itsrakesh/Portfolio/mockup_tgV6QfEef.png?ik-sdk-version=javascript-1.4.3&updatedAt=1665843870324",
+    repoName: "",
+    liveUrl: "https://home.tickle.life",
+    techs: [
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg",
+        title: "NextJs",
+        bg: "#fff",
+      },
+      {
+        url: "https://pbs.twimg.com/profile_images/1244925541448286208/rzylUjaf_400x400.jpg",
+        title: "Chakra-UI",
+      },
+    ],
+  },
+  {
+    title: "Orbit Admin Panel",
+    image:
+      "https://ik.imagekit.io/itsrakesh/Portfolio/1x-pika-1664022683697_1Tpq8iO3D.png?ik-sdk-version=javascript-1.4.3&updatedAt=1664022735970",
+    repoName: "",
+    liveUrl: "https://orbit-itsrakesh.vercel.app/",
+    techs: [
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        title: "React",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        title: "NodeJS",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+        title: "ExpressJS",
+        bg: "#fff",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+        title: "MongoDB",
+      },
+    ],
+  },
+  {
+    title: "Blog",
+    image:
+      "https://ik.imagekit.io/itsrakesh/Portfolio/blog-mockup_8n6Z-Xpxy.png?ik-sdk-version=javascript-1.4.3&updatedAt=1656583697436",
+    repoName: "Blog",
+    liveUrl: "https://blog.itsrakesh.co/",
+    techs: [
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg",
+        title: "NextJs",
+        bg: "#fff",
+      },
+      {
+        url: "https://pbs.twimg.com/profile_images/1244925541448286208/rzylUjaf_400x400.jpg",
+        title: "Chakra-UI",
+      },
+      {
+        url: "https://pbs.twimg.com/profile_images/1240607161639632896/65lOOz86_400x400.jpg",
+        title: "GraphCMS",
+      },
+    ],
+  },
+  {
+    title: "CampusMate",
+    image:
+      "https://ik.imagekit.io/itsrakesh/Portfolio/campusmate-mockup_zrODWFS6-3-.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1644130718344",
+    repoName: "Campus-Mate",
+    liveUrl: "https://campusmate-356fc.web.app/home",
+    videoId: "l6fuGTzPLi0",
+    techs: [
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        title: "React",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        title: "NodeJS",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+        title: "ExpressJS",
+        bg: "#fff",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+        title: "MongoDB",
+      },
+    ],
+  },
+  {
+    title: "Silver Screen",
+    image:
+      "https://ik.imagekit.io/itsrakesh/Portfolio/mockup_hisLcRKt5UiX.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1644130353877",
+    repoName: "silverScreen-frontend",
+    liveUrl: "https://silverscreen-2129f.web.app",
+    videoId: "kfCDwYprPhE",
+    techs: [
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        title: "React",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        title: "NodeJS",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+        title: "ExpressJS",
+        bg: "#fff",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+        title: "MongoDB",
+      },
+    ],
+  },
+  {
+    title: "Portfolio",
+    image:
+      "https://ik.imagekit.io/itsrakesh/Portfolio/mockup_2yl1LdmUH.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1644125260329",
+    repoName: "Portfolio",
+    liveUrl: "https://itsrakesh.co",
+    videoId: "ISLFpwYuQaA",
+    techs: [
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+        title: "React",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+        title: "NodeJS",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+        title: "ExpressJS",
+        bg: "#fff",
+      },
+      {
+        url: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+        title: "Bootstrap",
+      },
+    ],
+  },
+];
+
+module.exports = projects;
